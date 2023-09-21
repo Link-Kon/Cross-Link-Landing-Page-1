9cfcedb60150d09a2477f7eb1caec2dd972ba6e1
DiegoHZ5 <49615431+Diego5HZ@users.noreply.github.com>
2023-09-21 02:51:58 -0500

    avance

---
+++ CrossLinkLandingPage/CrossLinkLandingPage/Pages/Dashboard.razor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace CrossLinkLandingPage.Pages
+{
+    partial class Dashboard
+    {
+        private string actualEmail { get; set; }
+        private string actualSubject { get; set; }
+        private string actualBody { get; set; }
+
+        private Task SendMail()
+        {
+            try
+            {
+                //cuxpgfoybnifhhlz
+
+                actualEmail = string.Empty;
+                actualSubject= string.Empty;
+                actualBody = string.Empty;
+
+                return Task.CompletedTask;
+            }
+            catch (Exception ep)
+            {
+                Console.WriteLine("failed to send email with the following error:");
+                Console.WriteLine(ep.Message);
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
